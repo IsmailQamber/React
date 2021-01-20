73bc0d7ffabe5f0a95f45e1d12faaa9b834d283c
IsmailQamber <ism-you-95@hotmail.com>
2021-01-20 17:13:57 +0300

    done this

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import products from "./products.js";
 
 function App() {
+  const PictureSource = "./guitarimg.jpg";
+  document.title = "ScrapGuitar";
+
+  const productList = products.map((product) => (
+
+          <tr>
+            <td><img className = "center" src={product.image} /></td> 
+            <td>{product.name}</td>
+              <td> {product.price} </td>
+          </tr>
+
+  ));
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p className = "App"> 
+         Welcome to the one and only used guitars shop in the country, in which you can find good equipment that suits your budget. We can go as low as your budget can go. 
+      </p>
+        <img src={PictureSource} className="center" alt="logo" />
+        <h3>Our products</h3>
+        <table>
+          {productList}
+        </table>
       </header>
     </div>
   );
---
+++ src/products.js
@@ -0,0 +1,18 @@
+import blackGuitar from "./BlackVguitar.jpg"
+import pinknGuitar from "./pinkGuitar.jpg"
+const products =[
+    {
+        image: pinknGuitar,
+        name : "Pink guitar",
+        price : "10BD",
+    },
+    {
+        image : blackGuitar,
+        name : "Black v guitar",
+        price : "15BD",
+
+    },
+
+
+];
+export default products;
